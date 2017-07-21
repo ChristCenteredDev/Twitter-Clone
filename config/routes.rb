9019e1f330152b3873a27e7cd922f7fa33bc38e6
William A Mooney <wmooney1984@yahoo.com>
2017-07-21 10:32:00 +0000

    Update: Fixed the Profile Nav Bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+  
   devise_for :users
+  # Define Root URL
   root 'pages#index'
-
-  get 'home' => 'pages#home'
-
-  get 'user/:id' => 'pages#profile'
-
-  get 'explore' => 'pages#explore'
+  
+  # Define Routes for Pages
+  get '/home' => 'pages#home' # override default routes.
+  get '/user/:id' => 'pages#profile'
+  get '/explore' => 'pages#explore'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
