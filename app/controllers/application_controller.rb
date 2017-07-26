4a30f1f7efbcfea3e99a1282b615a0f8882b7e65
William A Mooney <wmooney1984@yahoo.com>
2017-07-26 12:14:09 +0000

    Update: Finished the Landing Page Forms & Polished the GUI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,24 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   
+  helper_method :resource_name, :resource, :devise_mapping, :resource_class
+
+  def resource_name
+    :user
+  end
+ 
+  def resource
+    @resource ||= User.new
+  end
+
+  def resource_class
+    User
+  end
+ 
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+  
   # protect the database, while allowing these fields to be updated.
   protected 
 
